1aebaf589a1c531ca33ffc1badf16f729eb8a045
𝘝𝘐𝘙𝘌𝘕𝘋𝘙𝘈 <194282754+AnyaXZero@users.noreply.github.com>
2025-06-13 15:50:22 +0530

    Update ping.py

--- AnieXEricaMusic/plugins/tools/ping.py
+++ AnieXEricaMusic/plugins/tools/ping.py
@@ -4,7 +4,6 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from AnieXEricaMusic import app
-from AnieXEricaMusic.core.call.py import ping
 from AnieXEricaMusic.utils import bot_sys_stats
 from AnieXEricaMusic.utils.decorators.language import language
 from AnieXEricaMusic.utils.inline import supp_markup
