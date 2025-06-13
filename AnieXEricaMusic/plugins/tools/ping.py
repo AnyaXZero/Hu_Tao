e94b460346449f94e58839aa0cfaf5f3458639a5
𝘝𝘐𝘙𝘌𝘕𝘋𝘙𝘈 <194282754+AnyaXZero@users.noreply.github.com>
2025-06-13 11:38:16 +0530

    Update ping.py

--- AnieXEricaMusic/plugins/tools/ping.py
+++ AnieXEricaMusic/plugins/tools/ping.py
@@ -3,11 +3,11 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
 
-from BrandrdXMusic import app
-from BrandrdXMusic.core.call import Hotty
-from BrandrdXMusic.utils import bot_sys_stats
-from BrandrdXMusic.utils.decorators.language import language
-from BrandrdXMusic.utils.inline import supp_markup
+from AnieXEricaMusic import app
+from AnieXEricaMusic.core.call import Hotty
+from AnieXEricaMusic.utils import bot_sys_stats
+from AnieXEricaMusic.utils.decorators.language import language
+from AnieXEricaMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
