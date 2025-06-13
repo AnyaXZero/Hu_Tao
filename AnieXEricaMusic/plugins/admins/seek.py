ede86ef63751d7e16f81750009d1bc6c4ebee198
𝘝𝘐𝘙𝘌𝘕𝘋𝘙𝘈 <194282754+AnyaXZero@users.noreply.github.com>
2025-06-13 11:54:31 +0530

    Update seek.py

--- AnieXEricaMusic/plugins/admins/seek.py
+++ AnieXEricaMusic/plugins/admins/seek.py
@@ -56,7 +56,7 @@ async def seek_comm(cli, message: Message, _, chat_id):
     if "index_" in file_path:
         file_path = playing[0]["vidid"]
     try:
-        await Hotty.seek_stream(
+        await AnieXEricaMusic.seek_stream(
             chat_id,
             file_path,
             seconds_to_min(to_seek),
