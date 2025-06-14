ce3ab7b26fb0c04523699970af16e25137fcf3bf
𝘝𝘐𝘙𝘌𝘕𝘋𝘙𝘈 <194282754+AnyaXZero@users.noreply.github.com>
2025-06-14 18:38:01 +0530

    Goodbye.py

--- AnieXEricaMusic/plugins/tools/fonts_base.py
+++ AnieXEricaMusic/plugins/tools/fonts_base.py
@@ -1,42 +1,9 @@
-class Fonts:
-    @staticmethod
-    def sans(text): return "𝙎𝙖𝙣𝙨 " + text
-    @staticmethod
-    def slant_san(text): return "𝘚𝘢𝘯𝘴 " + text
-    @staticmethod
-    def sim(text): return "𝖲𝖺𝗇𝗌 " + text
-    @staticmethod
-    def serif(text): return "𝐒𝐞𝐫𝐢𝐟 " + text
-    @staticmethod
-    def bold_cool(text): return "𝑺𝒆𝒓𝒊𝒇 " + text
-    @staticmethod
-    def cool(text): return "𝑆𝑒𝑟𝑖𝑓 " + text
-    @staticmethod
-    def script(text): return "𝓈𝒸𝓇𝒾𝓅𝓉 " + text
-    @staticmethod
-    def bold_script(text): return "𝓼𝓬𝓻𝓲𝓹𝓽 " + text
-    @staticmethod
-    def smallcap(text): return "sᴍᴀʟʟ cᴀᴘs " + text
-    @staticmethod
-    def circle_dark(text): return "🅒︎🅘︎🅡︎🅒︎🅛︎🅔︎🅨 " + text
-    @staticmethod
-    def circles(text): return "Ⓒ︎Ⓘ︎Ⓡ︎Ⓒ︎Ⓛ︎Ⓔ︎Ⓢ︎ " + text
-    @staticmethod
-    def bold_gothic(text): return "𝕲𝖔𝖙𝖍𝖎𝖈 " + text
-    @staticmethod
-    def gothic(text): return "𝔊𝔬𝔱𝔥𝔦𝔠 " + text
-    @staticmethod
-    def tiny(text): return "ᵗⁱⁿʸ " + text
-    @staticmethod
-    def outline(text): return "𝕆𝕦𝕥𝕝𝕚𝕟𝕖 " + text
-    @staticmethod
-    def comic(text): return "ᑕOᗰIᑕ " + text
-    @staticmethod
-    def special(text): return "🇸 🇵 🇪 🇨 🇮 🇦 🇱  " + text
-    @staticmethod
-    def square(text): return "🅂🅀🅄🄰🅇🅴🅂 " + text
-    @staticmethod
-    def dark_square(text): return "🆂︎🆀︎🆄︎🅰︎🆁︎🅴︎🆂︎ " + text
-    @staticmethod
-    def andalucia(text): return "ꪖꪀᦔꪖꪶꪊᥴ𝓲ꪖ " + text
-    # Add the rest as needed...
+from pyrogram import filters
+from pyrogram.types import Message
+from AnieXEricaMusic import app
+
+
+@app.on_message(filters.left_chat_member)
+async def goodbye(_, message: Message):
+    name = message.left_chat_member.mention
+    await message.reply_text(f"👀 {name} has left **{message.chat.title}**.\ɴᴡᴇ'ʟʟ ᴍɪꜱꜱ ʏᴏᴜ!")
