4a38feb40a27a802520c93dbf67eb25986ed44d1
𝘝𝘐𝘙𝘌𝘕𝘋𝘙𝘈 <194282754+AnyaXZero@users.noreply.github.com>
2025-06-14 15:43:49 +0530

    Update mmf.py

--- AnieXEricaMusic/plugins/tools/mmf.py
+++ AnieXEricaMusic/plugins/tools/mmf.py
@@ -34,9 +34,9 @@ async def drawText(image_path, text):
     i_width, i_height = img.size
 
     if os.name == "nt":
-        fnt = "arial.ttf"
+        fnt = "font.ttf"
     else:
-        fnt = "./Alya/assets/default.ttf"
+        fnt = "./AnieXEricaMusic/assets/mmf.ttf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
